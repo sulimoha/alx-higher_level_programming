e120444484457ab325380925376495760b3076ab
Mohamed <sulimmoh@gmail.com>
2022-09-19 19:42:44 -0700

    Betty fix

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -4,26 +4,27 @@
 * check_cycle - checks if a singly linked list has a cycle in it.
 * @list:  the head node of the list need to be checked
 *
-Return 0 (no cycle), 1 (cycle)
+*Return: 0 (no cycle), 1 (cycle)
 */
-int check_cycle(listint_t *list);
+int check_cycle(listint_t *list)
 {
-    listint_t  *current;
-    listint_t  *post
+	listint_t  *current;
+	listint_t  *post;
 
-    if(!list)
-    {
-        return (0);
-    }
-    current = list;
-    post = list->next;
-    while(post && post->next)
-    {
-        if (current == post)
-        {
-            return (1);
-        }
-        current = current->next;
-        post = post->next->next;
-    }
-    return (0);
+	if (!list)
+	{
+		return (0);
+	}
+	current = list;
+	post = list->next;
+	while (post && post->next)
+	{
+		if (current == post)
+		{
+			return (1);
+		}
+		current = current->next;
+		post = post->next->next;
+	}
+	return (0);
+}
