e3c7ba8853df4bf92501719d2a530f1e489e77c0
Mohamed <sulimmoh@gmail.com>
2022-09-26 18:29:28 -0500

    6-print_matrix_integer.py

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        noSpace = True
+        for column in row:
+            if noSpace == False:
+                print(" ", end="")
+            else:
+                noSpace = False
+            print("{:d}".format(column), end="")
+        print()
