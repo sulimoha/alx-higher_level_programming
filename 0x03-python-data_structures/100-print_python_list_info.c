2ec14cb23c7e530ed4afb6a87275bee0c772be53
Mohamed <sulimmoh@gmail.com>
2022-09-27 18:24:18 -0500

    100-print_python_list_info.c

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,23 @@
+#include "/usr/include/python3.4/pyhon.h"
+#include "/usr/include/python3.4/listobject.h"
+#include "/usr/include/python3.4/object.h"
+#include <stdio.h>
+
+void print_python_list_info(PyObject *p);
+{
+    int i = 0;
+    int len_list = 0;
+    pyobject *element;
+    pylistObject *clone = (pyListObject *) p;
+
+    len_list = py_SIZE(p);
+    printf("[*] size of the python List = %d\n", len_list");
+    printf("[*] Allocated = %d\n", (int) clone->allocated);
+
+    for (; i< len_list; ++i)
+    {
+        element = pyList_GET_ITEM(p, i);
+        printf("Element %d: %s\n", i, item->ob_type->tp_name);
+    }
+    return;
+}
