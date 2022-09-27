ed95b541b01058a562d300892ca0d4bc33be1df3
Mohamed <sulimmoh@gmail.com>
2022-09-26 20:06:33 -0500

    10-divisible_by_2.py

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,1 +1,9 @@
-#!/bin/usr/python3
+#!usr/bin/python3
+def divisible_by_2(my_list=[]):
+    logic_list = []
+    for i in range(len(my_list)):
+        if my_list[i] % 2 == 0:
+            logic_list.append(True)
+        else:
+            logic_list.append(False)
+    return logic_list
