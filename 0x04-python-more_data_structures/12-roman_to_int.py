77890af05cf57d8e6b251050ae03f5d6cba3d042
Mohamed <sulimmoh@gmail.com>
2022-09-28 18:13:57 -0500

    12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,13 +3,13 @@ def roman_to_int(roman_string):
     roman_dictionary = {"I": 1, "IV": 4, "V": 5, "IX": 9, "X": 10, "XL": 40,
                         "L": 50, "XC": 90, "C": 100, "CD": 400, "D": 500,
                         "CM": 900, "M": 1000}
-    if not roman_string.isalpha() or roman_string is None:
+    if roman_string.isalpha() is False or roman_string is None:
         return 0
     sum = 0
     for k, v in roman_dictionary.items():
         if roman_string.upper() == k:
-            return roman_dictionary[roman_string.upper()]
+            return v
         for char in roman_string.upper():
             if char == k:
-                sum = sum + roman_dictionary[(char)]
+                sum = sum + v
     return sum
