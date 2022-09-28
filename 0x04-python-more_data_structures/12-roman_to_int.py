d47b2b08f834f93cede849a8fc13bcc531a8398b
Mohamed <sulimmoh@gmail.com>
2022-09-28 18:41:51 -0500

    12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -5,11 +5,15 @@ def roman_to_int(roman_string):
                         "CM": 900, "M": 1000}
     if roman_string.isalpha() is False or roman_string is None:
         return 0
+    upper_roman_string = roman_string.upper()
     sum = 0
     for k, v in roman_dictionary.items():
-        if roman_string.upper() == k:
+        if upper_roman_string == k:
             return v
-        for char in roman_string.upper():
-            if char == k:
+        for i in range(len(upper_roman_string)):
+            if upper_roman_string[i:i + 2] == k:
                 sum = sum + v
+            else:
+                if upper_roman_string[i] == k:
+                    sum = sum + v
     return sum
