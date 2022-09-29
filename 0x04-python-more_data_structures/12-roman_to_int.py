e2536ebde21f7eb275b0a94e1a363387dcdbe92f
Mohamed <sulimmoh@gmail.com>
2022-09-28 20:56:57 -0500

    12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,19 +1,17 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    roman_dictionary = {"I": 1, "IV": 4, "V": 5, "IX": 9, "X": 10, "XL": 40,
-                        "L": 50, "XC": 90, "C": 100, "CD": 400, "D": 500,
-                        "CM": 900, "M": 1000}
-    if roman_string.isalpha() is False or roman_string is None:
-        return 0
-    upper_roman_string = roman_string.upper()
-    sum = 0
-    for k, v in roman_dictionary.items():
-        if upper_roman_string == k:
-            return v
-        for i in range(len(upper_roman_string)):
-            if upper_roman_string[i:i + 2] == k:
-                sum = sum + v
-            else:
-                if upper_roman_string[i] == k:
-                    sum = sum + v
-    return sum
+    roman_letters = [
+        ['I', 1], ['V', 5], ['X', 10], ['L', 50], ['C', 100],
+        ['D', 500], ['M', 1000]
+    ]
+    num = 0
+    last = 0
+    for letter in roman_string:
+        for elem in roman_letters:
+            if letter == elem[0]:
+                if last == 0 or last >= elem[1]:
+                    num += elem[1]
+                elif last < elem[1]:
+                    num += elem[1] - (last * 2)
+                last = elem[1]
+    return num
