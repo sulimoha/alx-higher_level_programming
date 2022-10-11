ca871fa227b71656640db9d21420e56974d387b8
Mohamed <sulimmoh@gmail.com>
2022-10-11 17:40:34 -0500

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Task1"""
+
+
+class Square:
+    """Represents a class, with a size."""
+    def __init__(self, size):
+        """Initializes the data."""
+        self.__size = size
