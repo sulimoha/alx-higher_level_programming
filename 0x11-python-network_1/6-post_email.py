0472dcaec52b7c937d7658d2ad7a46c512a41623
Mohamed <sulimmoh@gmail.com>
2023-01-17 19:16:09 -0600

    Write a Python script that takes in a URL and an email address, sends a POST request to the passed URL with the email as a parameter, and finally displays the body of the response.

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
+"""Takes in a URL and an email address, sends a
+POST request to the passed URL with the email
+as a parameter, and finally displays the body
+of the response.
 """
-Script that takes in a URL and an email address, sends a POST request to the/
-passed URL
-with the email as a parameter, and finally displays the body of the response
-"""
-import requests
-from sys import argv
 
-if __name__ == '__main__':
+from sys import argv
+import requests
+
+
+if __name__ == "__main__":
     payload = {'email': argv[2]}
-    r = requests.post(argv[1], data=payload)
-    print(r.text)
+    req = requests.post(argv[1], data=payload)
+
+    print(req.text)
